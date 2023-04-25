5fcfb434d156ae839a5048d2ce8064d521c01ef9
adil-nisar <adilnisar@gmail.com>
2023-04-25 14:30:53 +0500

    make section cta

--- components/global/cta/index.tsx
+++ components/global/cta/index.tsx
@@ -5,7 +5,7 @@ const Cta = () => {
       <div className="container mx-auto">
         <div className="flex flex-col text-center  mb-14 ">
           <p className="xl:text-5xl lg:text-4xl md:text-3xl lg:font-bold text-xl font-medium title-font md:mb-4 mb-7 text-[#ffff] mt-16 px-4 xl:max-w-[1770]">Get Started On Content Writing Campaigns That Bring Better Results!
-    </p>
+          </p>
           <p className=" mx-auto leading-5 text-base xl:text-2xl md:text-lg text-md px-4 font-normal text-rgba[0,0,0,0.7] opacity-90">To determine which content writing service will suit your needs, explore what we offer at our content writing services company.</p>
         </div>
         <div className="flex sm:justify-center justify-center">
