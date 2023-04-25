96b581e35844abb994a4a3f55d29e11a7fff7df9
adil-nisar <adilnisar@gmail.com>
2023-04-25 14:13:46 +0500

    make cta section

--- components/global/cta/index.tsx
+++ components/global/cta/index.tsx
@@ -1,7 +1,21 @@
 
-const Subscription = () => { 
-    return <section>Subscription</section>;
+const Cta = () => { 
+    return(
+      <section className="body-font bg-[#389583]">
+      <div className="container mx-auto">
+        <div className="flex flex-col text-center  mb-14 ">
+          <p className="xl:text-5xl lg:text-4xl md:text-3xl lg:font-bold text-xl font-medium title-font md:mb-4 mb-7 text-[#ffff] mt-16 px-4 xl:max-w-[1770]">Get Started On Content Writing Campaigns That Bring Better Results!
+    </p>
+          <p className=" mx-auto leading-5 text-base xl:text-2xl md:text-lg text-md px-4 font-normal text-rgba[0,0,0,0.7] opacity-90">To determine which content writing service will suit your needs, explore what we offer at our content writing services company.</p>
+        </div>
+        <div className="flex sm:justify-center justify-center">
+    
+    <a className="mb-24 lg:px-20 lg:py-4 md:px-10 md:py-3  px-9 py-2  rounded-[59px] text-[#ffff] bg-cover lg:text-2xl lg:font-bold md:text-xl text-md font-medium"   href='tel:+1(347)-795-0173'style={{backgroundImage:'url("Button (2).png")' }}>+1(347)-795-0173</a>
+    </div>
+      </div>
+    </section>
+    );
   };
   
-  export default Subscription;
+  export default Cta;
   
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import Cta from "@/components/global/cta";
+
  
 
 export default function Home() {
   return (
     <main>
-       
+       <Cta/>
     </main>
   )
 }
