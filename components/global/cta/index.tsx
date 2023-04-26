61018c1d7681357e435861db4ad848b8352b802d
adilleilani <adilnasir@leilanitech.com>
2023-04-26 00:51:55 -0700

    ready cta

--- components/global/cta/index.tsx
+++ components/global/cta/index.tsx
@@ -4,13 +4,13 @@ const Cta = () => {
       <section className="body-font bg-[#389583] w-full">
       <div className=" mx-auto">
         <div className="flex flex-col text-center  mb-14 ">
-          <p className="xl:text-5xl lg:text-4xl md:text-3xl lg:font-bold text-xl font-medium title-font md:mb-4 mb-7 text-[#ffff] mt-16 px-4 ">Get Started On Content Writing Campaigns That Bring Better Results!
+          <p className="xl:text-4xl lg:text-2xl  md:text-xl lg:font-medium text-xl font-medium title-font md:mb-4 mb-7 text-color-secondary mt-16 px-4 leading-8">Get Started On Content Writing Campaigns That Bring Better Results!
           </p>
-          <p className=" mx-auto leading-5 text-base xl:text-2xl md:mt-5 xl:font-normal md:text-lg px-4 font-normal text-rgba[0,0,0,0.7] opacity-90">To determine which content writing service will suit your needs, explore what we offer at our content writing services company.</p>
+          <p className=" mx-auto leading-5 text-base xl:text-xl md:mt-5 xl:font-normal md:text-md md:px-8 px-4 font-normal text-rgba[0,0,0,0.7] opacity-90">To determine which content writing service will suit your needs, explore what we offer at our content writing services company.</p>
         </div>
         <div className="flex sm:justify-center justify-center">
     
-    <a className="mb-24 lg:px-20 lg:py-4 md:px-10 md:py-3  px-9 py-2  rounded-[59px] text-[#ffff] bg-cover lg:text-2xl lg:font-bold md:text-xl text-md font-medium"   href='tel:+1(347)-795-0173'style={{backgroundImage:'url("Button (2).png")' }}>+1(347)-795-0173</a>
+    <a className="mb-24 lg:px-20 lg:py-4 md:px-10 md:py-3  px-9 py-2  rounded-[59px] text-color-fourth bg-cover lg:text-xl lg:font-bold md:text-md text-md font-medium"   href='tel:+1(347)-795-0173'style={{backgroundImage:'url("Button (2).png")' }}>+1(347)-795-0173</a>
     </div>
       </div>
     </section>
