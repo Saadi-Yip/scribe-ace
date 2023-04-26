9fc015992ab0f01150b84e49d9104a098b495e02
Rehan Aamir <rehan@leilanitech.com>
2023-04-26 08:54:31 -0400

    Fotter Completed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
-import Header from "@/components/global/header";
-
- 
-
+import Footer from "@/components/global/footer"; 
 export default function Home() {
   return (
     <main>
-       <Header />
+      <Footer/>
     </main>
   )
 }
