f463edc94dadb3481c4eb6ec3db171f03e2368be
adilleilani <adilnasir@leilanitech.com>
2023-04-26 02:38:25 -0700

    make ready portfolio

---
+++ components/home/benefits/index.tsx
@@ -0,0 +1,83 @@
+const Benifits=()=>{
+    return(
+        <section className="text-gray-600 body-font bg-color-primary">
+        <div className="container px-5 md:py-24 py-14 mx-auto">
+          <h1 className="sm:text-4xl text-2xl font-bold title-font text-center text-color-secondary mb-20">Benefits Of Our Content
+          </h1>
+          <div className="flex flex-wrap  sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+            <div className="p-4 md:w-1/2 flex flex-col ">
+              <div className="flex">
+              <div className="xl:w-20 xl:h-20 md:w-14 md:h-14 w-12 h-12 inline-flex items-center justify-center rounded-full  text-indigo-500 mb-4 flex-shrink-0">
+             <img src="heads.png" alt="" />
+              </div>
+              <div className="flex-grow pl-6">
+                <h2 className="text-color-secondary text-xl title-font font-bold mb-2 px-3">Professional In-house Team</h2>
+                <p className="leading-6 max-w-xl lg:text-lg text-sm font-normal lg:font-medium text-color-fifth opacity-70 px-3">Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.</p>
+              </div>
+              </div>
+      
+              <div className="flex md:mt-16 mt-10">
+              <div className="xl:w-20 xl:h-20 md:w-14 md:h-14 w-12 inline-flex items-center justify-center rounded-full  text-indigo-500 mb-4 flex-shrink-0">
+              <img src="time.png" alt="" />
+      
+              </div>
+              <div className="flex-grow pl-6">
+                <h2 className="text-color-secondary text-xl title-font font-bold mb-2 px-3">Fast Delivery</h2>
+                <p className="leading-6 max-w-xl lg:text-lg text-sm font-normal lg:font-medium text-color-fifth opacity-70 px-3">We offer a fast delivery service to make your life easier. Running a business seems to eat up time, but not having time shouldn‘t mean that you sacrifice your content. Neither should not having the resources. You can get the quality content you envisioned having without worrying about the time or resources you need to dedicate to content writing. Let us handle it with a fast delivery turnaround time to a high standard.
+      </p>
+              </div>
+              </div>
+      
+              <div className="flex md:mt-16 mt-10">
+              <div className="xl:w-20 xl:h-20 md:w-14 md:h-14 w-12 h-12 inline-flex items-center justify-center rounded-full  text-indigo-500 mb-4 flex-shrink-0">
+              <img src="heads.png" alt="" />
+      
+              </div>
+              <div className="flex-grow pl-6">
+                <h2 className="text-color-secondary text-xl title-font font-bold mb-2 px-3">Professional In-house Team</h2>
+                <p className="leading-6 max-w-xl lg:text-lg text-sm font-normal lg:font-medium text-color-fifth opacity-70 px-3">Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.</p>
+              </div>
+              </div>
+          
+            
+            
+            </div>
+            <div className="p-4 md:w-1/2 flex flex-col">
+            <div className="flex ">
+              <div className="xl:w-20 xl:h-20 md:w-14 md:h-14 w-12 h-12 inline-flex items-center justify-center rounded-full  text-indigo-500 mb-4 flex-shrink-0">
+              <img src="setting.png" alt="" />
+      
+              </div>
+              <div className="flex-grow pl-6">
+                <h2 className="text-color-secondary text-xl title-font font-bold mb-2 px-3">Professional In-house Team</h2>
+                <p className="leading-6 text-base max-w-xl lg:text-lg text-sm font-normal lg:font-medium text-color-fifth opacity-70 px-3">Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.</p>
+              </div>
+              </div>
+              <div className="flex md:mt-16 mt-10">
+              <div className="xl:w-20 xl:h-20 md:w-14 md:h-14 w-12 h-12 inline-flex items-center justify-center rounded-full  text-indigo-500 mb-4 flex-shrink-0">
+              <img src="World.png" alt="" />
+      
+              </div>
+              <div className="flex-grow pl-6">
+                <h2 className="text-color-secondary text-xl title-font font-bold mb-2 px-3">Professional In-house Team</h2>
+                <p className="leading-6 max-w-xl lg:text-lg text-sm font-normal  lg:font-medium text-color-fifth opacity-70 px-3">Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.</p>
+              </div>
+              </div>
+              <div className="flex md:mt-16 mt-10">
+              <div className="xl:w-20 xl:h-20 md:w-14 md:h-14 w-12 h-12 inline-flex items-center justify-center rounded-full  text-indigo-500 mb-4 flex-shrink-0">
+              <img  src="graph.png" alt="" />
+      
+              </div>
+              <div className="flex-grow pl-6">
+                <h2 className="text-color-secondary text-xl title-font font-bold mb-2 px-3">Professional In-house Team</h2>
+                <p className="leading-6  max-w-xl lg:text-lg text-sm font-normal lg:font-medium text-color-fifth opacity-70 px-3">When you need content, but you are facing a resource-based issue, such as an in-house content team that doesn’t have SEO skills or enough resources, external support from our content writing team will enable you to boost your SEO the right way, and in turn, improve your sales and leads. By using our SEO content writing services, with time, you will easily see the difference our SEO-friendly page titles, meta descriptions, web copy, and blogs make to your search rankings.</p>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+}
+
+export default Benifits
