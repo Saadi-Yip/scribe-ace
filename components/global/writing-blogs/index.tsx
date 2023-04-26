ac297c79f64ebe0de7866d41d9bf17814d71bc55
Saadi-Yip <saadshabbir@leilanitech.com>
2023-04-26 18:14:57 +0500

    props added to writing-blogs

--- components/global/writing-blogs/index.tsx
+++ components/global/writing-blogs/index.tsx
@@ -12,20 +12,24 @@ import "swiper/css";
 import "swiper/css/navigation";
 import useDimensions from "@/hooks/use-dimensions";
 
-const Blogs = () => {
+type Prop = {
+  data: { title: string; details: string; url: string }[];
+};
+
+const Blogs = (props: Prop) => {
   return (
     <section className="container xl:max-w-[70%] lg:max-w-[90%] max-w-[80%] mx-auto mt-2 py-10 relative">
       <h5 className="md:text-3xl text-color-third text-2xl font-bold mb-2 text-center">
         Our Elite Book Writing Service Expertise
       </h5>
-      <Slides />
+      <Slides data = {props.data}/>
     </section>
   );
 };
 
 export default Blogs;
 
-const Slides = (props: any) => {
+const Slides = (props: Prop) => {
   const { width } = useDimensions();
   const swiperRef = useRef<any>(null);
   return (
@@ -40,141 +44,36 @@ const Slides = (props: any) => {
         }}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <div className="carousel-item mx-auto rounded">
-            <div className="flex flex-wrap justify-center bg-transparent pb-4">
-              <div className="shadow-xl swiper-head testimonials-bg rounded-3xl mt-12 ">
-                <div className="header">
-                  <Image
-                    alt=""
-                    src="/portfolio-1.png"
-                    className="p-0 w-full rounded-2xl"
-                    width={473}
-                    height={414}
-                  />
-                </div>
-                <div className="content pb-4">
-                  <h5 className="text-[#05396B] leading-5 text-lg lg:text-md xl:text-lg pt-10 pl-6 lg:pl-4 xl:pl-6 font-bold">
-                    Lead Designers 12
-                  </h5>
-                  <p className="text-[#000000b3] md:text-sm lg:text-base xl:text-lg text-left p-6 lg:p-4 xl:p-6 text-lg sm:text-sm leading-6 sm:leading-5 md:leading-6 tracking-tight shadow-uyjh67\1741xl font-medium">
-                    Our elite book writers for hire have the experience required
-                    to write informative content; therefore, you can rely on us
-                    to write an accurate and factual book for you.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="carousel-item mx-auto rounded">
-            <div className="flex flex-wrap justify-center bg-transparent pb-4">
-              <div className="shadow-xl swiper-head testimonials-bg rounded-3xl mt-12  ">
-                <div className="header">
-                  <Image
-                    alt=""
-                    src="/portfolio-2.png"
-                    className="p-0 w-full rounded-2xl"
-                    width={473}
-                    height={414}
-                  />
-                </div>
-                <div className="content pb-4">
-                  <h5 className="text-[#05396B] leading-5 text-lg lg:text-md xl:text-lg pt-10 pl-6 lg:pl-4 xl:pl-6 font-bold">
-                    Lead Designers
-                  </h5>
-                  <p className="text-[#000000b3] md:text-sm lg:text-base xl:text-lg text-left p-6 lg:p-4 xl:p-6 text-lg sm:text-sm leading-6 sm:leading-5 md:leading-6 tracking-tight shadow-uyjh67\1741xl font-medium">
-                    Our elite book writers for hire have the experience required
-                    to write informative content; therefore, you can rely on us
-                    to write an accurate and factual book for you.
-                  </p>
+
+        {
+          props.data.map((item:any, i:number) => {
+            return <SwiperSlide key = {i.toString()}>
+            <div className="carousel-item mx-auto rounded">
+              <div className="flex flex-wrap justify-center bg-transparent pb-4">
+                <div className="shadow-xl swiper-head testimonials-bg rounded-3xl mt-12 ">
+                  <div className="header">
+                    <Image
+                      alt=""
+                      src={item.url}
+                      className="p-0 w-full rounded-2xl"
+                      width={473}
+                      height={414}
+                    />
+                  </div>
+                  <div className="content pb-4">
+                    <h5 className="text-[#05396B] leading-5 text-lg lg:text-md xl:text-lg pt-10 pl-6 lg:pl-4 xl:pl-6 font-bold">
+                      {item.title}
+                    </h5>
+                    <p className="text-[#000000b3] md:text-sm lg:text-base xl:text-lg text-left p-6 lg:p-4 xl:p-6 text-lg sm:text-sm leading-6 sm:leading-5 md:leading-6 tracking-tight shadow-uyjh67\1741xl font-medium">
+                      {item.details}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="carousel-item mx-auto rounded">
-            <div className="flex flex-wrap justify-center bg-transparent pb-4">
-              <div className="shadow-xl swiper-head testimonials-bg rounded-3xl mt-12  ">
-                <div className="header">
-                  <Image
-                    alt=""
-                    src="/portfolio-3.png"
-                    className="p-0 w-full rounded-2xl"
-                    width={473}
-                    height={414}
-                  />
-                </div>
-                <div className="content pb-4">
-                  <h5 className="text-[#05396B] leading-5 text-lg lg:text-md xl:text-lg pt-10 pl-6 lg:pl-4 xl:pl-6 font-bold">
-                    Lead Designers
-                  </h5>
-                  <p className="text-[#000000b3] md:text-sm lg:text-base xl:text-lg text-left p-6 lg:p-4 xl:p-6 text-lg sm:text-sm leading-6 sm:leading-5 md:leading-6 tracking-tight shadow-uyjh67\1741xl font-medium">
-                    Our elite book writers for hire have the experience required
-                    to write informative content; therefore, you can rely on us
-                    to write an accurate and factual book for you.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="carousel-item    mx-auto rounded">
-            <div className="flex flex-wrap justify-center bg-transparent pb-4">
-              <div className="shadow-xl swiper-head testimonials-bg rounded-3xl mt-12  ">
-                <div className="header">
-                  <Image
-                    src="/portfolio-1.png"
-                    className="p-0 w-full rounded-2xl"
-                    alt=""
-                    width={473}
-                    height={414}
-                  />
-                </div>
-                <div className="content pb-4">
-                  <h5 className="text-[#05396B] leading-5 text-lg lg:text-md xl:text-lg pt-10 pl-6 lg:pl-4 xl:pl-6 font-bold">
-                    Lead Designers
-                  </h5>
-                  <p className="text-[#000000b3] md:text-sm lg:text-base xl:text-lg text-left p-6 lg:p-4 xl:p-6 text-lg sm:text-sm leading-6 sm:leading-5 md:leading-6 tracking-tight shadow-uyjh67\1741xl font-medium">
-                    Our elite book writers for hire have the experience required
-                    to write informative content; therefore, you can rely on us
-                    to write an accurate and factual book for you.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="carousel-item  mx-auto rounded">
-            <div className="flex flex-wrap justify-center bg-transparent pb-4">
-              <div className="shadow-xl swiper-head testimonials-bg rounded-3xl mt-12">
-                <div className="header">
-                  <Image
-                    alt=""
-                    src="/portfolio-2.png"
-                    className="p-0 w-full rounded-2xl"
-                    width={473}
-                    height={414}
-                  />
-                </div>
-                <div className="content pb-4">
-                  <h5 className="text-[#05396B] leading-5 text-lg lg:text-md xl:text-lg pt-10 pl-6 lg:pl-4 xl:pl-6 font-bold">
-                    Lead Designers
-                  </h5>
-                  <p className="text-[#000000b3] md:text-sm lg:text-base xl:text-lg text-left p-6 lg:p-4 xl:p-6 text-lg sm:text-sm leading-6 sm:leading-5 md:leading-6 tracking-tight shadow-uyjh67\1741xl font-medium">
-                    Our elite book writers for hire have the experience required
-                    to write informative content; therefore, you can rely on us
-                    to write an accurate and factual book for you.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+          })
+        }
       </Swiper>
       <div>
         <button
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,38 @@
- 
 import Blogs from "@/components/global/writing-blogs";
 
- 
+//&& Blogs Props &&//
+const blogs_data = [
+  {
+    title: "Fiction",
+    details: "Our elite book writers for hire have the experience required to write informative content; therefore, you can rely on us to write an accurate and factual book for you.",
+    url: '/portfolio-1.png'
+  },
+  {
+    title: "Fiction",
+    details: "Our elite book writers for hire have the experience required to write informative content; therefore, you can rely on us to write an accurate and factual book for you.",
+    url: '/portfolio-2.png'
+  },
+  {
+    title: "Fiction",
+    details: "Our elite book writers for hire have the experience required to write informative content; therefore, you can rely on us to write an accurate and factual book for you.",
+    url: '/portfolio-3.png'
+  },
+  {
+    title: "Fiction",
+    details: "Our elite book writers for hire have the experience required to write informative content; therefore, you can rely on us to write an accurate and factual book for you.",
+    url: '/portfolio-1.png'
+  },
+  {
+    title: "Fiction",
+    details: "Our elite book writers for hire have the experience required to write informative content; therefore, you can rely on us to write an accurate and factual book for you.",
+    url: '/portfolio-2.png'
+  },
+];
 
 export default function Home() {
   return (
     <main>
-       <Blogs/>
+      <Blogs data={blogs_data} />
     </main>
-  )
+  );
 }
