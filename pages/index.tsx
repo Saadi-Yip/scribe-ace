a0e4c0cbef6d03a3ae11fb8c696d37bb8007f789
numanleilani <numanabid@leilanitech.com>
2023-04-27 06:10:58 -0400

    resolve issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import Blogs from "@/components/global/writing-blogs";
 import Portfolio from "@/components/global/portfolio";
 import Pricing from "@/components/global/pricing";
 import Footer from "@/components/global/footer";
+import Head from "next/head";
 
 //&& Blogs Props &&//
 const blogs_data = [
@@ -56,28 +57,59 @@ const listing = [
 ];
 
 //Statistics Props
-const data = [ 
+const data = [
   { title: "Satisfied Clients", count: 1874 },
   { title: "Years of Experience", count: 7 },
   { title: "Projects", count: 1874 },
-  { title: "Increased ROI", count: 187 }
-]
+  { title: "Increased ROI", count: 187 },
+];
 
 //cta props
 const cta_title = "Industry-Leading Professional Elite Book Writers For Hire";
-const cta_details = "To determine which content writing service will suit your needs, explore what we offer at our content writing services company."
- 
+const cta_details =
+  "To determine which content writing service will suit your needs, explore what we offer at our content writing services company.";
+
 // Benefits Props
-const benefits1 = [ 
-  {url:'heads.png', title: "Professional In-house Team", details: 'Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.' },
-  {url:'time.png', title: "Fast Delivery", details: 'Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.' },
-  {url:'heads.png', title: "Unique and High-Quality Content", details: 'Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.' },
-]
+const benefits1 = [
+  {
+    url: "heads.png",
+    title: "Professional In-house Team",
+    details:
+      "Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.",
+  },
+  {
+    url: "time.png",
+    title: "Fast Delivery",
+    details:
+      "Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.",
+  },
+  {
+    url: "heads.png",
+    title: "Unique and High-Quality Content",
+    details:
+      "Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.",
+  },
+];
 const benefits2 = [
-  {url:'setting.png', title: "Professional In-house Team", details: 'Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.' },
-  {url:'World.png', title: "An extensive Team of UK and US writers", details: 'Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.' },
-  {url:'graph.png', title: "Boost Your SEO and Your Sales and Leads", details: 'Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.' },
-]
+  {
+    url: "setting.png",
+    title: "Professional In-house Team",
+    details:
+      "Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.",
+  },
+  {
+    url: "World.png",
+    title: "An extensive Team of UK and US writers",
+    details:
+      "Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.",
+  },
+  {
+    url: "graph.png",
+    title: "Boost Your SEO and Your Sales and Leads",
+    details:
+      "Behind all of our content is a team of 4 word masters. Our copy editors work closely with our team of writers to produce the custom content with all the specifications you want, whether a short and sweet brief or a detailed, in-depth one is provided. As a content writing services company we have helped more than 200 hundred different clients with their content writing in the last two years alone, and we want this number to grow.",
+  },
+];
 
 // Steps Props
 const steps = [
@@ -220,35 +252,68 @@ const prices = [
 ];
 
 // Testimonials
-const testimonials = [ 
-  {url:'person-1.avif', name: "David Weasley", role:'Website Developer', details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ' },
-  {url:'person-2.jfif', name: "Emma Watson", role:'Seo Expert', details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ' },
-  {url:'person-3.avif', name: "Serious Black", role:'User Interface', details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ' },
-  {url:'person-4.avif', name: "Dominic Torreto", role:'Graphic Designer', details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ' },
-  {url:'person-5.jpg', name: "Albert", role:'Call Center Agent', details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ' },
-
-]
-
+const testimonials = [
+  {
+    url: "person-1.avif",
+    name: "David Weasley",
+    role: "Website Developer",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ",
+  },
+  {
+    url: "person-2.jfif",
+    name: "Emma Watson",
+    role: "Seo Expert",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ",
+  },
+  {
+    url: "person-3.avif",
+    name: "Serious Black",
+    role: "User Interface",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ",
+  },
+  {
+    url: "person-4.avif",
+    name: "Dominic Torreto",
+    role: "Graphic Designer",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ",
+  },
+  {
+    url: "person-5.jpg",
+    name: "Albert",
+    role: "Call Center Agent",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus nibh mauris, nec turpis orci lectus maecenas. Suspendisse sed magna eget nibh in turpis. Consequat duis diam lacus arcu. Faucibus venenatis felis id augue sit cursus pellentesque enim ",
+  },
+];
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <Hero
-        heading={heading}
-        description={description}
-        listing={listing}
-        data = {data}
-      />
-      <Services />
-      <Cta title = {cta_title} details = {cta_details}/>
-      <Steps steps = {steps}/>
-      <Benifits benefits1 = {benefits1} benefits2 = {benefits2}/>
-      <Blogs data={blogs_data} />
-      <Portfolio />
-      <Pricing prices = {prices}/>
-      <Testimonials testimonials = {testimonials}/>
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main>
+        <Header />
+        <Hero
+          heading={heading}
+          description={description}
+          listing={listing}
+          data={data}
+        />
+        <Services />
+        <Cta title={cta_title} details={cta_details} />
+        <Steps steps={steps} />
+        <Benifits benefits1={benefits1} benefits2={benefits2} />
+        <Blogs data={blogs_data} />
+        <Portfolio />
+        <Pricing prices={prices} />
+        <Testimonials testimonials={testimonials} />
+        <Footer />
+      </main>
+    </>
   );
 }
