349fce3a66f4a9accd5cc6090dbf7cbdc5e60640
adilleilani <adilnasir@leilanitech.com>
2023-04-26 22:30:58 -0700

    remove extra class

--- components/global/services/index.tsx
+++ components/global/services/index.tsx
@@ -19,7 +19,7 @@ const Services = (props: Prop) => {
   return (
     <section>
 
-      <div className="flex justify-between">
+      <div className="">
           <Content data = {props.data}/>
         </div>
 
